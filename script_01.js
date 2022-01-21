46fa57a781f4c393953b90109f0669233844430c
Peter <schmidt.multimedia@gmail.com>
2022-01-21 11:48:42 +0100

    Transponierung / Aufsummierung

--- script_01.js
+++ script_01.js
@@ -36,10 +36,37 @@
 // }
 
 /* 02b. For-Schleife für Array-Index (Iteration)*/
-let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
-for (let i = 0; i < arr.length; i++) {
-     output(arr[i]);
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) {
+//      output(arr[i]);
+// }
+
+ /*********   Überlegungen - Transponierung **********/
+
+/* 
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+let a = 0; // Anfangswert
+for (let i = 0; i <= 10; i++) {
+    output("in der loop: " + a);
+    a = a + 1;
 }
+output("nach der loop: " + a);
+
 
 
 /*** 01. Funktionalität mit Einzelparametern */
@@ -78,6 +105,34 @@ function getSentenceArr(arr) {
 
     return str;
 }
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+
+// output(getSentenceArr2(["Ich","bin","Max","Mütze"]));
+function getSentenceArr2(arr) {
+
+    const gap   = " ";
+    const punct = ".";
+
+    for (let i = 0; i < arr.length; i++) {
+        output(arr[i]);
+    }
+
+
+    let str   =     arr[0] + gap +
+                    arr[1] + gap +
+                    arr[2] + gap +
+                    arr[3] + 
+                    punct;
+
+    return str;
+}
+
+
+
+
 
 
 // Modul: Ausgabe in Konsole : Test
