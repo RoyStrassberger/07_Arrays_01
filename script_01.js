a09d1f96b250d6c53917a0a3eb55cec8d71c9b52
Peter <schmidt.multimedia@gmail.com>
2022-01-21 10:24:30 +0100

    01a. Funktionalität mit Array 1

--- script_01.js
+++ script_01.js
@@ -4,17 +4,17 @@
 
 /*** 02. Theorie: Array */
 
-let arr;
-// arr = new Array(); // Konstruktor
-arr = []; // Literal
-arr = [2,4,7,11];
-arr = [false,true,true];
-arr = ["Ich","bin","Maxine","Mützerich"];
+// let arr;
+// // arr = new Array(); // Konstruktor
+// arr = []; // Literal
+// arr = [2,4,7,11];
+// arr = [false,true,true];
+// arr = ["Ich","bin","Maxine","Mützerich"];
 
-output(arr);
-output(arr.length); // Anzahl der Elemente
-output(arr[0]); // Index (1. Pos. im Array)
-output(arr[arr.length-1]) // letzte Pos.
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index (1. Pos. im Array)
+// output(arr[arr.length-1]) // letzte Pos.
 
 
 
@@ -34,6 +34,25 @@ function getSentence(word1,word2,word3,word4) {
                     word2 + gap +
                     word3 + gap +
                     word4 + 
+                    punct;
+
+    return str;
+}
+
+/*** 01a. Funktionalität mit Array 1  */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n] ::Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation  semantisches Problem --> numerisches Problem
+output(getSentenceArr(["Ich","bin","Max","Mütze"]));
+function getSentenceArr(arr) {
+
+    const gap   = " ";
+    const punct = ".";
+    let str   =     arr[0] + gap +
+                    arr[1] + gap +
+                    arr[2] + gap +
+                    arr[3] + 
                     punct;
 
     return str;
