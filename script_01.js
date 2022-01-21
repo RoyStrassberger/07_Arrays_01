3136a1978cbb1e19b761de9554dbe9ff38580dbf
Peter <schmidt.multimedia@gmail.com>
2022-01-21 08:56:56 +0100

    01. Funktionalität mit Einzelparametern

--- script_01.js
+++ script_01.js
@@ -3,6 +3,23 @@
 /* Die Satzbau-Maschine | Arrays */
 
 
+/*** 01. Funktionalität mit Einzelparametern */
+// --> "Ich bin Max Mütze."
+
+output(getSentence("Ich","bin","Max","Mütze"));
+output(getSentence("Ich","bin","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4) {
+
+    const gap   = " ";
+    const punct = ".";
+    const str   =   word1 + gap +
+                    word2 + gap +
+                    word3 + gap +
+                    word4 + 
+                    punct;
+
+    return str;
+}
 
 
 
